4f925843b6b4ecd86f4f4c64e36d8debdbf14013
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-27 15:07:39 +0530

    Added: Remove avoidable word from the phrase

--- Program.cs
+++ Program.cs
@@ -1,10 +1,30 @@
-﻿namespace HashTables
+﻿using HashTable;
+
+namespace HashTables
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome tho the Hash Table");
+
+            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] words = paragraph.Split(' ');
+
+            HashTable<string, int> frequency = new HashTable<string, int>(words.Length);
+
+            foreach (string word in words)
+            {
+                int count = frequency.Get(word);
+                if (count == default(int))
+                {
+                    count = 0;
+                }
+                frequency.Add(word, count + 1);
+            }
+
+            frequency.Remove("avoidable");
+            frequency.Display();
         }
 
     }
